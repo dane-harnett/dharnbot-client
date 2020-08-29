7f5d2335290bec7048457150df249550e7ea0362
Dane Harnett <dane_harnett@flightcentre.com>
2020-08-29 13:13:33 +1000

    Style with styled-components

--- src/components/ChatCommand.tsx
+++ src/components/ChatCommand.tsx
@@ -1,24 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledChatCommand = styled.span`
+  background-color: #7b2529;
+  border-radius: 4px;
+  color: #f0deba;
+  display: inline-block;
+  padding: 4px;
+  margintop: 4px;
+`;
 
 interface ChatCommandProps {
   command: string;
 }
 
 const ChatCommand = ({ command }: ChatCommandProps) => {
-  return (
-    <span
-      style={{
-        backgroundColor: "#7b2529",
-        borderRadius: "4px",
-        color: "#f0deba",
-        display: "inline-block",
-        padding: "4px",
-        marginTop: "4px",
-      }}
-    >
-      {command}
-    </span>
-  );
+  return <StyledChatCommand>{command}</StyledChatCommand>;
 };
 
 export default ChatCommand;
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { formatDuration, intervalToDuration } from "date-fns";
+import styled from "styled-components";
+
+const Label = styled.div`
+  box-sizing: border-box;
+  width: 400px;
+  background-color: rgba(0, 0, 0, 0.75);
+  color: #fff;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  bottom: 0;
+  padding: 4px;
+`;
 
 const Countdown = () => {
   const [remaining, setRemaining] = useState("");
@@ -14,23 +27,7 @@ const Countdown = () => {
       setRemaining(formatDuration(duration));
     }, 500);
   }, [setRemaining, targetDate]);
-  return (
-    <div
-      style={{
-        boxSizing: "border-box",
-        width: 400,
-        backgroundColor: "rgba(0,0,0,0.75)",
-        color: "#fff",
-        position: "fixed",
-        display: "flex",
-        alignItems: "center",
-        bottom: 0,
-        padding: 4,
-      }}
-    >
-      End of stream in {remaining}
-    </div>
-  );
+  return <Label>End of stream in {remaining}</Label>;
 };
 
 export default Countdown;
